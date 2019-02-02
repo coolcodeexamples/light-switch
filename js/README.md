576c66dc3cda52f4ffa94e66f2589e50dd5de867
Homa Wong (unional) <homawong@gmail.com>
2019-02-02 00:24:52 -0800

    add js

---
+++ js/README.md
@@ -0,0 +1,8 @@
+# light-switch in JavaScript
+
+The use cases are described in the `src/index.spec.js`
+
+## Setup
+
+This project uses `yarn`.
+To setup, run `yarn install`.
---
+++ js/src/index.spec.js
@@ -0,0 +1,24 @@
+
+test.todo('when the light is off, flip the switch will turn on the light')
+
+test.todo('when the light is on, flip the switch will turn off the light')
+
+test.todo('one switch can control multiple lights at the same time')
+
+test.todo('two switches can control the same light')
+
+test.todo('when both switches are flipped at about the same time, the light will switch on/off')
+
+test.todo('if the light is dimmable, the switch can control its brightness')
+
+test.todo('when the light is off, turning up brightness will turn it on')
+
+test.todo('when the light is on, turning brightness down to zero will turn it off')
+
+test.todo('the switch can display how bright the light is')
+
+test.todo(`if the light is not dimmable,
+should the switch disable the brightness setting,
+or it will have the same effect as turning the light on and off?`)
+
+test.todo(`the switch can control individual lights`)
